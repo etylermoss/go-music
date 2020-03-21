8f4a2960e110020f2d73c8c813a59ba6d2e5888d
Eden Tyler-Moss <eden@etylermoss.com>
2020-03-21 16:14:37 +0000

    Set devServer port based on global-onfig.json.

--- client/webpack.dev_server.js
+++ client/webpack.dev_server.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const dev = require('./webpack.dev.js');
+const globalConfig = require('../global-config.json');
 
 module.exports = merge(dev, {
 	devServer: {
 		contentBase: path.join(__dirname, 'build'),
 		compress: true,
-		port: 5001
+		port: globalConfig.port + 1
 	}
 });
