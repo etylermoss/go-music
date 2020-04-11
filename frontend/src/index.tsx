7fa36eafcf795a9795abe74833570a8282aa4474
Eden Tyler-Moss <eden@etylermoss.com>
2020-04-11 22:19:57 +0100

    Removed initial slash from apiPath variable.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,15 +5,16 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 
 /* 1st party imports */
+import globalConfig from 'globalConfig';
 import { Greeting } from './hello';
 
 const client = new ApolloClient({
-	uri: 'http://localhost:5000/api/graphql'
+	uri: `http://${window.location.host}/${globalConfig.apiPath}/graphql`
 });
 
 const Root = (): JSX.Element => (
 	<ApolloProvider client={client}>
-		<Greeting compiler='TypeScript' framework='React'/>
+			<Greeting compiler='TypeScript' framework='React'/>
 	</ApolloProvider>
 );
 
